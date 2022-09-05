e3d6b6ea0c5e44c2a9327a39c1856fae29c5d694
rwang3 <rwang3@heygears.com>
2022-09-05 13:06:11 +0800

    增加cmake项目测试代码

---
+++ CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeProject1.cpp: 定义应用程序的入口点。
+//
+
+#include "CMakeProject1.h"
+#include <boost/any>
+using namespace std;
+
+int main()
+{
+	std::string str("Hello");
+	str.append("  CMake.");
+	cout << "str: " << str << endl;
+	return 0;
+}
