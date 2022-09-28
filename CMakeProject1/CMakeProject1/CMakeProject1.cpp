685ba1b007d4a5a444e85bf49e116a5344d30e78
Maxwrt <1374935261@qq.com>
2022-09-28 22:11:39 +0800

    update

--- CMakeProject1/CMakeProject1/CMakeProject1.cpp
+++ CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -47,11 +47,13 @@ void init_log()
 }
 int main(int argc, char *argv[])
 {
+#if 0
 	if (argc < 2)
 	{
 		cout << "Usage: CMakeProject1 path\n";
 		return 1;
 	}
+#endif
 	cout << argv[0] << " " << file_size(argv[0]) << endl;
 	string str("Hello");
 	str.append(" CMake.");
@@ -65,7 +67,7 @@ int main(int argc, char *argv[])
 	cout << "sum: " << sum(10, 11) << endl;
 
 	//调用 boost的filesystem
-	directory_entry dir_(argv[1]);
+	directory_entry dir_("./");
 	cout << "root_path:" << dir_.path().root_directory() << endl;
 	cout << "root_name:" << dir_.path().root_name() << endl;
 	cout << "root_path:" << dir_.path().root_path() << endl;
@@ -75,7 +77,7 @@ int main(int argc, char *argv[])
 	cout << "filename:" << dir_.path().filename() << endl;
 	cout << "extension: " << dir_.path().extension() << endl;
 	
-	path p(argv[1]);
+	path p("./");
 
 	try
 	{
@@ -113,6 +115,15 @@ int main(int argc, char *argv[])
 	BOOST_LOG_TRIVIAL(warning) << "A warning severity message";
 	BOOST_LOG_TRIVIAL(error) << "An error severity message";
 	BOOST_LOG_TRIVIAL(fatal) << "A fatal severity message";
-
+	/*
+	*	|    数据包         | CRC校验码 |
+	*	| 01 03 00 12 00 10 | 03 E4     |
+	*/
+	std::vector<uint8_t> array = { 0x01, 0x03, 0x00, 0x12, 0x00, 0x10 };
+	uint16_t cal_crc = Crc16ForModbus(array);
+	if (cal_crc == 0x03E4)
+		cout << "data correct\n";
+	else
+		cout << "data error\n";
 	return 0;
 }
--- CMakeProject1/CMakeProject1/utility/math_.h
+++ CMakeProject1/CMakeProject1/utility/math_.h
@@ -1,14 +1,18 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+using namespace std;
 #define DLL_IMPORT __declspec(dllimport)
 #define DLL_EXPORT __declspec(dllexport)
 
 #ifdef  BUILD_DLL
 	DLL_EXPORT	int sum(int a, int b);
 	DLL_EXPORT int minus(int a, int b);
+	DLL_EXPORT uint16_t Crc16ForModbus(const std::vector<uint8_t>& data);
 #else
 	DLL_IMPORT int sum(int a, int b);
 	DLL_IMPORT int minus(int a, int b);
+	DLL_IMPORT uint16_t Crc16ForModbus(const std::vector<uint8_t>& data);
 #endif //  BUILD_DLL
 
